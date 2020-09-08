acb0ed07037616f83c0866bb08d37e26de6ba96c
lalit@lalitadithya.com <lalit@lalitadithya.com>
2020-09-08 20:05:13 +0530

    Integrated key valut to save and fetch account key pem

--- LetsEncryptAzureCdn/ApplyOrRenewCertificate.cs
+++ LetsEncryptAzureCdn/ApplyOrRenewCertificate.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Threading.Tasks;
+using Certes;
+using Certes.Acme;
+using LetsEncryptAzureCdn.Helpers;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace LetsEncryptAzureCdn
@@ -8,9 +11,26 @@ namespace LetsEncryptAzureCdn
     public static class ApplyOrRenewCertificate
     {
         [FunctionName("ApplyOrRenewCertificate")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            AcmeContext acmeContext;
+
+            var secretHelper = new KeyVaultSecretHelper(Environment.GetEnvironmentVariable("KeyVaultName"));
+            var acmeAccountPem = await secretHelper.GetSecretAsync("AcmeAccountKeyPem");
+            if (string.IsNullOrWhiteSpace(acmeAccountPem))
+            {
+                acmeContext = new AcmeContext(WellKnownServers.LetsEncryptStagingV2);
+                await acmeContext.NewAccount(Environment.GetEnvironmentVariable("AcmeAccountEmail"), true);
+                var pem = acmeContext.AccountKey.ToPem();
+                await secretHelper.SetSecretAsync("AcmeAccountKeyPem", pem);
+            }
+            else
+            {
+                acmeContext = new AcmeContext(WellKnownServers.LetsEncryptStagingV2, KeyFactory.FromPem(acmeAccountPem));
+            }
+
         }
     }
 }
---
+++ LetsEncryptAzureCdn/Helpers/KeyVaultSecretHelper.cs
@@ -0,0 +1,46 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsEncryptAzureCdn.Helpers
+{
+    public class KeyVaultSecretHelper
+    {
+        private string keyVaultUri;
+        private SecretClient secretClient;
+
+        public KeyVaultSecretHelper(string keyVaultName)
+        {
+            keyVaultUri = $"https://{keyVaultName}.vault.azure.net";
+            secretClient = new SecretClient(new Uri(keyVaultUri), new VisualStudioCredential());
+        }
+
+        public async Task<string> GetSecretAsync(string secretName)
+        {
+            try
+            {
+                return (await secretClient.GetSecretAsync(secretName)).Value.Value;
+            }
+            catch (RequestFailedException e)
+            {
+                if (e.Status == 404)
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async Task SetSecretAsync(string secretName, string secretValue)
+        {
+            await secretClient.SetSecretAsync(secretName, secretValue);
+        }
+    }
+}
