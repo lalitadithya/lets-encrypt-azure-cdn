40debaaa4ae7d8b97995cdc2a8d8a70cb6ab3a0e
lalit@lalitadithya.com <lalit@lalitadithya.com>
2020-09-11 19:48:39 +0530

    Added model for input

---
+++ LetsEncryptAzureCdn/Models/CertificateRenewalInputModel.cs
@@ -0,0 +1,23 @@
+﻿namespace LetsEncryptAzureCdn.Models
+{
+    public class CertificateRenewalInputModel
+    {
+        public string DnsZoneResourceGroup { get; set; }
+        public string DnsZoneName { get; set; }
+
+        public string DomainName { get; set; }
+        public string CertificateCountryName { get; set; }
+        public string CertificateState { get; set; }
+        public string CertificateLocality { get; set; }
+        public string CertificateOrganization { get; set; }
+        public string CertificateOrganizationUnit { get; set; }
+
+        public string CdnProfileName { get; set; }
+        public string CdnEndpointName { get; set; }
+        public string CdnCustomDomainName { get; set; }
+        public string CdnResourceGroup { get; set; }
+
+        public string KeyVaultName { get; set; }
+        public string KeyVaultResourceGroup { get; set; }
+    }
+}
