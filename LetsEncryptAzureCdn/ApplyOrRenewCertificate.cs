f6d8cf471aeeee8e939c771d474faf2479c92f39
lalit@lalitadithya.com <lalit@lalitadithya.com>
2020-09-14 18:17:47 +0530

    Added logs

--- LetsEncryptAzureCdn/ApplyOrRenewCertificate.cs
+++ LetsEncryptAzureCdn/ApplyOrRenewCertificate.cs
@@ -30,16 +30,20 @@ namespace LetsEncryptAzureCdn
 
             foreach (var certifcate in certificateDetails)
             {
+                log.LogInformation($"Processing certificate - {certifcate.DomainName}");
                 AcmeContext acmeContext;
 
                 var secretHelper = new KeyVaultSecretHelper(certifcate.KeyVaultName);
                 var acmeAccountPem = await secretHelper.GetSecretAsync("AcmeAccountKeyPem");
                 if (string.IsNullOrWhiteSpace(acmeAccountPem))
                 {
+                    log.LogInformation("Acme Account not found.");
                     acmeContext = new AcmeContext(WellKnownServers.LetsEncryptV2);
                     await acmeContext.NewAccount(Environment.GetEnvironmentVariable("AcmeAccountEmail"), true);
                     var pem = acmeContext.AccountKey.ToPem();
+                    log.LogInformation("Acme account created");
                     await secretHelper.SetSecretAsync("AcmeAccountKeyPem", pem);
+                    log.LogInformation("Secret uploaded to key vault");
                 }
                 else
                 {
@@ -53,8 +57,10 @@ namespace LetsEncryptAzureCdn
                 {
                     domainName = domainName.Substring(1);
                 }
+                log.LogInformation($"Calculated domain name is {domainName}");
 
                 string keyVaultCertificateName = domainName.Replace(".", "");
+                log.LogInformation($"Getting expiry for {keyVaultCertificateName} in Key Vault certifictes");
                 var certificateExpiry = await certificateHelper.GetCertificateExpiryAsync(keyVaultCertificateName);
                 if (certificateExpiry.HasValue && certificateExpiry.Value.Subtract(DateTime.UtcNow).TotalDays > 7)
                 {
@@ -62,29 +68,37 @@ namespace LetsEncryptAzureCdn
                     continue;
                 }
 
+                log.LogInformation("Creating order for certificates");
                 var order = await acmeContext.NewOrder(new string[] { certifcate.DomainName });
                 var authorization = (await order.Authorizations()).First();
+                log.LogInformation("Authorization created");
 
                 string subscriptionId = Environment.GetEnvironmentVariable("SubscriptionId");
 
                 var dnsHelper = new DnsHelper(subscriptionId);
 
+                log.LogInformation("Fetching DNS authorization");
                 var dnsChallenge = await authorization.Dns();
                 var dnsText = acmeContext.AccountKey.DnsTxt(dnsChallenge.Token);
                 var dnsName = ("_acme-challenge." + domainName).Replace("." + certifcate.DnsZoneName, "").Trim();
+                log.LogInformation($"Got DNS challenge {dnsText} for {dnsName}");
 
                 var txtRecords = await dnsHelper.FetchTxtRecordsAsync(certifcate.DnsZoneResourceGroup, certifcate.DnsZoneName, dnsName);
 
                 if (txtRecords == null || !txtRecords.Contains(dnsText))
                 {
                     await dnsHelper.CreateTxtRecord(certifcate.DnsZoneResourceGroup, certifcate.DnsZoneName, dnsName, dnsText);
+                    log.LogInformation("Created DNS TXT records");
                 }
 
+                log.LogInformation("Waiting 60 seconds for DNS propagation");
                 await Task.Delay(60 * 1000);
 
+                log.LogInformation("Validating DNS challenge");
                 var challengeResult = await dnsChallenge.Validate();
                 while (challengeResult.Status.HasValue && challengeResult.Status.Value == Certes.Acme.Resource.ChallengeStatus.Pending)
                 {
+                    log.LogInformation("Validation is pending. Will retry in 1 second");
                     await Task.Delay(1 * 1000);
                     challengeResult = await dnsChallenge.Resource();
                 }
@@ -94,6 +108,8 @@ namespace LetsEncryptAzureCdn
                     log.LogError("Unable to validate challenge - {0} - {1}", challengeResult.Error.Detail, string.Join('~', challengeResult.Error.Subproblems.Select(x => x.Detail)));
                     return;
                 }
+
+                log.LogInformation("Challenge validated");
 
                 var privateKey = KeyFactory.NewKey(KeyAlgorithm.RS256);
                 var cert = await order.Generate(new CsrInfo
@@ -111,12 +127,18 @@ namespace LetsEncryptAzureCdn
                 string password = "abcd1234";
                 var pfx = pfxBuilder.Build(domainName, password);
 
+                log.LogInformation("Certificate built");
+
                 (string certificateName, string certificateVerison) = await certificateHelper.ImportCertificate(keyVaultCertificateName, pfx, password);
+
+                log.LogInformation("Certificate imported");
 
                 var cdnHelper = new CdnHelper(subscriptionId);
 
                 await cdnHelper.EnableHttpsForCustomDomain(certifcate.CdnResourceGroup, certifcate.CdnProfileName,
                     certifcate.CdnEndpointName, certifcate.CdnCustomDomainName, certificateName, certificateVerison, certifcate.KeyVaultName);
+
+                log.LogInformation("HTTPS enabling started");
             }
         }
     }
